bbdadf5cfda58638052ddaa6477e264302628c96
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2019-08-19 13:21:16 -0300

    refactoring main.go

---
+++ timedEvent/timedEvent.go
@@ -0,0 +1,42 @@
+package timedEvent
+
+import "sync"
+
+type TimedEvent struct {
+	Light    *sync.Mutex
+	Finished chan bool
+	Wg       *sync.WaitGroup
+}
+
+func (t *TimedEvent) IsEmpty() bool {
+	return t == nil
+}
+
+func NewTimedEvent() *TimedEvent {
+	finished := make(chan bool, 1)
+	light := &sync.Mutex{}
+	wg := &sync.WaitGroup{}
+
+	tEvent := &TimedEvent{
+		Light:    light,
+		Finished: finished,
+		Wg:       wg}
+
+	return tEvent
+
+}
+
+func (t *TimedEvent) CheckReceiveSignalNoHang() bool {
+	select {
+	case <-t.Finished:
+		return true
+	default:
+		return false
+	}
+}
+
+func (t *TimedEvent) TurnSwitch(finished chan bool, light *sync.Mutex) {
+	t.Light.Lock()
+	t.Finished <- true
+	t.Light.Unlock()
+}
